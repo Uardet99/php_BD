f7dd6e7fac1a65bcf00c5eed2bcbdbcc0b13c767
Uardet99 <eduardvalls17@gmail.com>
2022-04-13 23:00:43 +0200

    Consultas Preparadas

--- pdo.php
+++ pdo.php
@@ -1,16 +1,22 @@
 <?php
 
+$id = $_GET['id'];
+
 try {
     $conexion = new PDO ('mysql:host=localhost;dbname=prueba_datos', 'root', '');
     //echo "Conexion OK";
 
     // Metodo Query, para ejecutar consultas pequeñas
-    $resultado = $conexion -> query('SELECT * FROM usuarios WHERE id = 2');
+    /* $resultado = $conexion -> query('SELECT * FROM usuarios WHERE id = 2');
 
     foreach ($resultado as $fila) {
         echo $fila['ID']. ' - '. $fila['nombre'] . '<br>';
-    }
+    } */
 
+    $consulta = $conexion->prepare('INSERT INTO usuarios VALUES(null, "Edu")'); 
+    $consulta->execute();
+
+    
 }catch(PDOException $e) {
     echo "Error: " . $e -> getMessage();
 }
