05250fb034b19cb09cbe14e4ebf6252442e39a7d
Uardet99 <eduardvalls17@gmail.com>
2022-04-13 22:09:18 +0200

    Metodo Query

--- pdo.php
+++ pdo.php
@@ -2,7 +2,15 @@
 
 try {
     $conexion = new PDO ('mysql:host=localhost;dbname=prueba_datos', 'root', '');
-    echo "Conexion OK";
+    //echo "Conexion OK";
+
+    // Metodo Query, para ejecutar consultas pequeñas
+    $resultado = $conexion -> query('SELECT * FROM usuarios WHERE id = 2');
+
+    foreach ($resultado as $fila) {
+        echo $fila['ID']. ' - '. $fila['nombre'] . '<br>';
+    }
+
 }catch(PDOException $e) {
     echo "Error: " . $e -> getMessage();
 }
